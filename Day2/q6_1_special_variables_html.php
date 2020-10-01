e1bb0c043a5dfb49dc5683568ff105b29944e309
freakieDoom <prixcreater@gmail.com>
2020-10-01 16:05:53 +0530

    Assignments

--- Day2/q6_1_special_variables_html.php
+++ Day2/q6_1_special_variables_html.php
@@ -1,11 +1,12 @@
 <?php 
-
-	$a = $_GET['1'];
-
-	$b = $_GET['2'];
+	if (!empty($_GET[1])) {
 		
-	$c = $_GET['3'];
+		$a = $_GET['1'];
 
+		$b = $_GET['2'];
+			
+		$c = $_GET['3'];
+	}
  ?>
 <!DOCTYPE html>
 <html>
@@ -18,29 +19,31 @@
 	<h2 align="center">Enter Sides</h2>
 	<div align="center">
 		<form action="q6_1_special_variables_html.php">
-			<input type="number" name="1">
-			<input type="number" name="2">
-			<input type="number" name="3"><br><br>
+			<input type="number" name="1" min="0">
+			<input type="number" name="2" min="0">
+			<input type="number" name="3" min="0"><br><br>
 			<input type="submit" name="submit" value
 			="Check"><br><br>
 			<h1 colour="red">
 				<?php 
+					if (!empty($_GET[1])){
+							if ($a && $b && $c) {
+							if($a == $b && $b == $c){	
+								echo "Equilateral Triangle";
+							}
+							elseif ($a == $b || $b == $c || $a == $c) {
+									echo "Isosceles Triangle";
+							}
 
-					if ($a && $b && $c) {
-						if($a == $b && $b == $c){	
-							echo "Equilateral Triangle";
-						}
-						elseif ($a == $b || $b == $c || $a == $c) {
-								echo "Isosceles Triangle";
-						}
-
-						elseif ((($a**2 + $b**2) == $c**2) || (($b**2 + $c**2) == $a**2) || (($a**2 + $c**2) == $b**2)) {
-							echo "Right Angled And Scalene Triangle";
-						}
-						else{
-							echo "Scalene Triangle";
-						}
+							elseif ((($a**2 + $b**2) == $c**2) || (($b**2 + $c**2) == $a**2) || (($a**2 + $c**2) == $b**2)) {
+								echo "Right Angled And Scalene Triangle";
+							}
+							else{
+								echo "Scalene Triangle";
+							}
+						}	
 					}
+					
 
 				 ?>
 			</h1>
--- Day2/q6_2_special_variables_html.php
+++ Day2/q6_2_special_variables_html.php
@@ -1,11 +1,14 @@
 <?php 
-		$name = $_POST['name'];
-		$sub1 = $_POST['1'];	
-		$sub2 = $_POST['2'];
-		$sub3 = $_POST['3'];
-		$sub4 = $_POST['4'];
-		$sub5 = $_POST['5'];
-		$all = array($sub1,$sub2,$sub3,$sub4,$sub5);
+		if (!empty($_POST[1])) {
+			$name = $_POST['name'];
+			$sub1 = $_POST['1'];	
+			$sub2 = $_POST['2'];
+			$sub3 = $_POST['3'];
+			$sub4 = $_POST['4'];
+			$sub5 = $_POST['5'];
+			$all = array($sub1,$sub2,$sub3,$sub4,$sub5);	
+		}
+		
 	
 
  ?>
@@ -19,60 +22,58 @@
 		<form method="POST">
 			<input type="text" name="name" placeholder="Student's Name" required><br><br>
 			Marks Obtained Out of 100 <br><br>
-			<input type="number" name="1" placeholder="Subject 1" min="0" max="100"><br><br>
-			<input type="number" name="2" placeholder="Subject 2" min="0" max="100"><br><br>
-			<input type="number" name="3" placeholder="Subject 3" min="0" max="100"><br><br>
-			<input type="number" name="4" placeholder="Subject 4" min="0" max="100"><br><br>
-			<input type="number" name="5" placeholder="Subject 5" min="0" max="100"><br><br><br>
+			<input type="number" name="1" placeholder="Subject 1" min="0" max="100" required><br><br>
+			<input type="number" name="2" placeholder="Subject 2" min="0" max="100" required><br><br>
+			<input type="number" name="3" placeholder="Subject 3" min="0" max="100" required><br><br>
+			<input type="number" name="4" placeholder="Subject 4" min="0" max="100" required><br><br>
+			<input type="number" name="5" placeholder="Subject 5" min="0" max="100" required><br><br><br>
 			<input type="submit" name="submit" value="Get Data">		
 		</form>
-	</div>
+	</div>																						
 	<h2 align="left">
 			Name of Student :
 			<?php 
-				if($name){
-					echo strtoupper($name);	
+				if (!empty($_POST[1])){
+					if($name){
+						echo strtoupper($name);	
+					}	
 				}
 				
-
-			 ?>
+			?>
 	</h2>
 	<br>
 	<h3>
 		Marks in Each Subject <br>
 			<?php
-				if ($sub1) {
-					$i = 1;
-					foreach($all as $marks) {
-					echo nl2br("Subject ".($i)." : ".($marks)." Marks\n\n");
-						$i++;
-					}
-				 } 
-				
-
-			 ?>
+				if (!empty($_POST[1])){
+					if ($sub1) {
+						$i = 1;
+						foreach($all as $marks) {
+							echo nl2br("Subject ".($i)." : ".($marks)." Marks\n\n");
+							$i++;
+						}
+				 	}
+				}
+				 
+			?>
 		  <br>
 		 Total Marks Obtained : 
-		 <?php 
-
-		 	echo array_sum($all)." Marks";
-
-		  ?><br>
+		<?php 
+			if (!empty($_POST[1])){
+				echo array_sum($all)." Marks";	
+			}
+			
+		?><br>
 		  Total Marks : 500 Marks <br><br>
 		  <span style="border: 3px dotted white; padding: 10px; background: black; color: white">
 			  Percentage : 
-			  <?php 
-
-			  		echo ((array_sum($all)/500)*100)." %";
-
-			   ?>
+			  	<?php 
+			  		if (!empty($_POST[1])){
+			  			echo ((array_sum($all)/500)*100)." %";
+			  		}
+					
+			  	?>
 		   </span>
-
 	</h3>
-
-
-
-	
-
 </body>
 </html>
